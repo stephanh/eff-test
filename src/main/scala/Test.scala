ae53fff0ca0e4adc614846d7bd3330064c907560
Stephan Hoermann <stephan@Stephans-MBP.home.gateway>
2016-09-29 21:53:03 +1000

    Adds more type annotations.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -14,18 +14,18 @@ object Test {
 object Test2 {
   type Stack = Fx.fx2[KVStore, KVStore2]
 
-  val program = for {
+  val program: Eff[Stack, Option[String]] = for {
     x <- KVStore.program[Stack]
     y <- KVStore2.program[Stack]
   } yield y
 
   type Stack2 = Fx.fx6[KVStore, KVStore2, State[Map[String, String], ?], State[Map[String, Int], ?], Writer[String, ?], Writer[Int, ?]]
 
-  val x = program.into[Stack2]
-  val a = KVStoreInterpreter.runKVStore(KVStoreInterpreter2.runKVStore(x))
-    .evalState(Map.empty[String, String])
-    .evalState(Map.empty[String, Int])
-    .runWriter
-    .runWriter
+  val x: Eff[Stack2, Option[String]] = program.into[Stack2]
+  val a: ((Option[String], List[String]), List[Int])  = KVStoreInterpreter.runKVStore(KVStoreInterpreter2.runKVStore(x))
+    .evalState[Map[String, String]](Map.empty[String, String])
+    .evalState[Map[String, Int]](Map.empty[String, Int])
+    .runWriter[String]
+    .runWriter[Int]
     .run
 }
