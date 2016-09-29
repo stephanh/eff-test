e36c0835118c41f7b487254cbac37e198b08d766
Stephan Hoermann <stephan@Stephans-MBP.home.gateway>
2016-09-30 00:04:27 +1000

    More examples.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -26,11 +26,82 @@ object Test2 {
 
 
   val x: Eff[Stack2, Option[String]] = program.into[Stack2]
-  val a: ((Option[String], List[String]), List[Int]) =
-    KVStoreInterpreter.runKVStore[Stack3, Stack4, Option[String]](KVStoreInterpreter2.runKVStore[Stack2, Stack3, Option[String]](x))
+  // val a: ((Option[String], List[String]), List[Int]) =
+  //   KVStoreInterpreter.runKVStore[Stack3, Stack4, Option[String]](KVStoreInterpreter2.runKVStore[Stack2, Stack3, Option[String]](x))
+  //     .evalState[Map[String, String]](Map.empty[String, String])
+  //     .evalState[Map[String, Int]](Map.empty[String, Int])
+  //     .runWriter[String]
+  //     .runWriter[Int]
+  //     .run
+}
+
+object Test3 {
+  type Stack = Fx.fx2[KVStore, KVStore2]
+
+  val program: Eff[Stack, Option[String]] = for {
+    x <- KVStore.program[Stack]
+    y <- KVStore2.program[Stack]
+  } yield y
+
+  type Stack2 = Fx.fx4[KVStore, KVStore2, State[Map[String, Int], ?], Writer[Int, ?]]
+  type Stack3 = Fx.fx3[KVStore2, State[Map[String, Int], ?], Writer[Int, ?]]
+  type Stack4 = Fx.fx4[State[Map[String, Int], ?], Writer[Int, ?], State[Map[String, String], ?], Writer[String, ?]]
+
+  val a: Eff[Stack2, Option[String]] = program.into[Stack2]
+  val b: Eff[Stack3, Option[String]] = KVStoreInterpreter.runKVStore[Stack2, Stack3, Option[String]](a)
+  //val c: Eff[Stack4, Option[String]] = KVStoreInterpreter2.runKVStore[Stack3, Stack4, Option[String]](b)
+}
+
+object Test4 {
+  type Stack = Fx.fx2[KVStore, KVStore2]
+
+  val program: Eff[Stack, Option[String]] = for {
+    x <- KVStore.program[Stack]
+    y <- KVStore2.program[Stack]
+  } yield y
+
+  type Stack2 = Fx.fx4[KVStore, KVStore2, State[Map[String, Int], ?], Writer[Int, ?]]
+  type Stack3 = Fx.fx3[KVStore2, State[Map[String, Int], ?], Writer[Int, ?]]
+  type Stack4 = Fx.fx5[KVStore2, State[Map[String, Int], ?], Writer[Int, ?], State[Map[String, String], ?], Writer[String, ?]]
+  type Stack5 = Fx.fx4[State[Map[String, Int], ?], Writer[Int, ?], State[Map[String, String], ?], Writer[String, ?]]
+
+  val a: Eff[Stack2, Option[String]] = program.into[Stack2]
+  val b: Eff[Stack3, Option[String]] = KVStoreInterpreter.runKVStore[Stack2, Stack3, Option[String]](a)
+  val c: Eff[Stack4, Option[String]] = b.into[Stack4]
+  val d: Eff[Stack5, Option[String]] = KVStoreInterpreter2.runKVStore[Stack4, Stack5, Option[String]](c)
+  val x =
+    d
+      .evalState[Map[String, String]](Map.empty[String, String])
+      .evalState[Map[String, Int]](Map.empty[String, Int])
+      .runWriter[String]
+  //    .runWriter[Int]
+  //     .run
+}
+
+/** Same as Test4 but with runWriter[Int] uncommented. */
+object Test5 {
+  type Stack = Fx.fx2[KVStore, KVStore2]
+
+  // Runtime error
+  val program: Eff[Stack, Option[String]] = for {
+    x <- KVStore.program[Stack]
+    y <- KVStore2.program[Stack]
+  } yield y
+
+  type Stack2 = Fx.fx4[KVStore, KVStore2, State[Map[String, Int], ?], Writer[Int, ?]]
+  type Stack3 = Fx.fx3[KVStore2, State[Map[String, Int], ?], Writer[Int, ?]]
+  type Stack4 = Fx.fx5[KVStore2, State[Map[String, Int], ?], Writer[Int, ?], State[Map[String, String], ?], Writer[String, ?]]
+  type Stack5 = Fx.fx4[State[Map[String, Int], ?], Writer[Int, ?], State[Map[String, String], ?], Writer[String, ?]]
+
+  val a: Eff[Stack2, Option[String]] = program.into[Stack2]
+  val b: Eff[Stack3, Option[String]] = KVStoreInterpreter.runKVStore[Stack2, Stack3, Option[String]](a)
+  val c: Eff[Stack4, Option[String]] = b.into[Stack4]
+  val d: Eff[Stack5, Option[String]] = KVStoreInterpreter2.runKVStore[Stack4, Stack5, Option[String]](c)
+  val x =
+    d
       .evalState[Map[String, String]](Map.empty[String, String])
       .evalState[Map[String, Int]](Map.empty[String, Int])
       .runWriter[String]
       .runWriter[Int]
-      .run
+  //     .run
 }
